4bcf6343da305d1ec8d686d8c275e8a2242a8eb1
whereislima <reach.lima@gmail.com>
2021-04-02 13:09:47 -0700

    completed

--- calc/app.py
+++ calc/app.py
@@ -1,4 +1,41 @@
 # Put your app in here.
-from flask import flask
+from flask import Flask, request
+from operations import add, sub, mult, div
 
-spp = Flask(__name__)
+app = Flask(__name__)
+
+@app.route('/add')
+def do_add():
+    """Adds a and b and returns result as the body."""
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    result = add(a, b)
+
+    return str(result)
+
+@app.route('/sub')
+def do_sub():
+    """Same, subtracting b from a."""
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    result = sub(a, b)
+
+    return str(result)
+
+@app.route('/mult')
+def do_mult():
+    """ Same, multiplying a and b."""
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    result = mult(a, b)
+
+    return str(result)
+
+@app.route('/div')
+def do_div():
+    """Same, dividing a by b."""
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    result = div(a, b)
+
+    return str(result)
--- greet/app.py
+++ greet/app.py
@@ -1,3 +1,20 @@
-from flask import flask
+from flask import Flask
 
-spp = Flask(__name__)
+app = Flask(__name__)
+
+@app.route('/welcome')
+def welcome():
+    """Returns "welcome" """
+    return "welcome"
+
+
+@app.route('/welcome/home')
+def welcome_home():
+    """Returns "welcome home" """
+    return "welcome home"
+
+
+@app.route('/welcome/back')
+def welcome_back():
+    """Returns "welcome back" """
+    return "welcome back"
